9303cdf0582da01f26073edd053d1f8da7e434ae
FZ2161 <126457897+FZ2161@users.noreply.github.com>
2024-01-05 16:14:18 +0100

    Add files via upload

---
+++ egzamin2/video.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl3.css">
+    <title>Video On Demand</title>
+</head>
+<body>
+    <div id="grid">
+        <div id="lewy">
+            <h1>
+                Internetowa wypożyczalnia filmów
+            </h1>
+        </div>
+        <div id="prawy">
+            <table>
+                <tr>
+                    <td>Kryminał</td>
+                    <td>Horror</td>
+                    <td>Przygodowy</td>
+                </tr>
+                <tr>
+                    <td>20</td>
+                    <td>30</td>
+                    <td>20</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
+    <div id="listaPolecamy">
+        <h3>
+            Polecamy +skrypt1
+        </h3>
+    </div>
+    <div id="listaFilmyFantastyczne">
+        <h3>
+            Filmy fantastyczne +skrypt2
+        </h3>
+    </div>
+
+    <div id="stopka">
+        <form method="POST">
+            Stronę wykonał:<a href="mailto:ja@poczta.com"> PESEL</a>
+            
+        </form>
+        
+    </div>
+
+</body>
+</html>
